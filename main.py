0d35957a89f50f3ce25c36da101255b3d6e94fcc
Dylan Haworth <vindylhaw@gmail.com>
2020-09-20 18:19:07 -0700

    first run

---
+++ main.py
@@ -0,0 +1,9 @@
+from discord.ext import commands, tasks
+
+bday = commands.Bot(command_prefix = ['++'])
+
+@bot.event
+async def on_ready():
+    print('LIBERTY PRIME ONLINE')
+
+bday.run('NzU3NDA4NzI1NDAwMTU4MjQ5.X2f92A.dXieo499US-zuZHmhi88CE7MhUM')
